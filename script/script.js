30ec192ecd058f7cdc5156978af49b54de7139ab
LindoufoGA <glindoufo1@gmail.com>
2024-01-17 14:47:41 -0300

    first commit

---
+++ script/script.js
@@ -0,0 +1,33 @@
+// SLIDER
+let currentSlide = 0;
+
+function changeSlide(index) {
+  currentSlide = index;
+  updateSlider();
+}
+
+function updateSlider() {
+  const slides = document.querySelector('.slides');
+  const slideWidth = document.querySelector('.slide').offsetWidth;
+
+  slides.style.transform = `translateX(${-currentSlide * slideWidth}px)`;
+}
+
+// Adiciona a transição automática a cada 3 segundos
+setInterval(() => {
+  if (currentSlide < 5) {
+    currentSlide++;
+  } else {
+    currentSlide = 0;
+  }
+  updateSlider();
+}, 3000);
+
+// MENU-MOBILE
+const menuMobile = document.getElementById("btn-mobile");
+
+function abrirMenu() {
+  const menu = document.getElementById("nav");
+  menu.classList.toggle("ativo");
+}
+menuMobile.addEventListener("click", abrirMenu);
